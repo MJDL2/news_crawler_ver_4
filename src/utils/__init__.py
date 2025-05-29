9e1b9d64d40ab5804d17023d3d131b95284a1952
MJDL2 <55268330+MJDL2@users.noreply.github.com>
2025-05-29 20:31:38 +0900

    Add utils package init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,8 @@
 """
-유틸리티 모듈
+유틸리티 패키지 초기화
 """
 
-from .config import get_config, Config
+from .config import get_config, config
 from .file_saver import FileSaver
 
-__all__ = ['get_config', 'Config', 'FileSaver']
+__all__ = ['get_config', 'config', 'FileSaver']
